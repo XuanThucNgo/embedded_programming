44795cadcd755a4bb12b440068a268f7d95b8b19
thuc <xuanthuc15072003@gmail.com>
2024-03-26 02:41:21 +0700

    Struct and CMCSIS

--- lesson11/main.c
+++ lesson11/main.c
@@ -32,13 +32,31 @@ int main(){
   u32e = (uint32_t)u16b + u21d;
   
   u16b = 100U;
+  //s32 = 10 - u16b;
   s32 = 10 - (int16_t)u16b;
+  // when mix a signed and unsigned operands both are promoted to "unsigned int" 
+//=> the result is "unsigned int". Subsequently, the result is converted to signed 32-bit 
+//   because s32 is 32-bit match with 32 bit-ARM => So it fills completely the s32 variable
+//   but 16 bit not same 
+
   
   //if(u32e > -1)   // -1 always promotes the signed to unsigned int => -1 -> 0xFFFFFFFF => fix that
   if((int32_t)u32e > -1)
     u8a = 1U;
   else
     u8a = 0U;
+  
+  u8a = 0xffU;
+  /*
+  if (~u8a == 0x00U) {
+    u8b = 1U;
+  }
+  0xffU will be promoted gets promoted to 0x000000ff (8 to 32 bit promotion). 
+  therefore ~u8a will be 0xffffff00 which is not 0
+  => fix that*/
+  if ((uint8_t)~u8a == 0x00U) {
+    u8b = 1U;
+  }
   
   SYSCTL_RCGCGPIO_R  |= (1U << 5); //Turn on port F  
   SYSCTL_GPIOHBCTL_R |= (1U << 5); //Enable AHB for GPIOF
