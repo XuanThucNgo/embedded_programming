8741886268a418532e6aa6bcc82b8befd8acde57
thuc <xuanthuc15072003@gmail.com>
2024-04-01 02:51:15 +0700

    ok

---
+++ lesson14/startup_tm4c.c
@@ -0,0 +1,7 @@
+/*startup code for TM4C MCU*/
+
+int const __vector_table[] @ ".intvec" = {
+  0x20004000,
+  0x9
+};
+  
---
+++ lesson21/tm4c123-keil/Objects/system_tm4c123gh6pm.d
@@ -0,0 +1,6 @@
+./objects/system_tm4c123gh6pm.o: ..\ek-tm4c123gxl\system_TM4C123GH6PM.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  ..\ek-tm4c123gxl\TM4C123GH6PM.h ..\CMSIS\Include\core_cm4.h \
+  ..\CMSIS\Include\cmsis_version.h ..\CMSIS\Include\cmsis_compiler.h \
+  ..\CMSIS\Include\cmsis_armclang.h ..\CMSIS\Include\mpu_armv7.h \
+  ..\ek-tm4c123gxl\system_TM4C123GH6PM.h
